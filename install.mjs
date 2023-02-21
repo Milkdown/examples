e4f834970b235a1ba3f16fe02d125b01f526c2ec
Mirone Saul <Saul-Mirone@outlook.com>
2023-02-21 11:38:08 +0800

    Bump versions

---
+++ install.mjs
@@ -0,0 +1,20 @@
+import { exec } from 'child_process';
+import fs from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { promisify } from 'util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const exampleDirs = fs.readdirSync(__dirname, { withFileTypes: true }).filter(dirent => dirent.isDirectory() && !dirent.name.startsWith('.'));
+
+await Promise.all(exampleDirs.map(async (dir) => {
+    const { name } = dir;
+    const path = resolve(__dirname, name)
+    console.log(`[${name}]: Start install.`);
+    const { stderr } = await promisify(exec)(`cd ${path} && pnpm install`)
+    if (stderr) console.error(stderr);
+    console.log(`[${name}]: Install complete.`);
+}));
+
