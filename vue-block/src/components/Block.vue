3b35c12fb7b97093875e228dd0b35ab255a8d656
Mirone <Saul-Mirone@outlook.com>
2024-07-02 02:25:08 +0800

    update block plugin examples

--- vue-block/src/components/Block.vue
+++ vue-block/src/components/Block.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { BlockProvider } from "@milkdown/plugin-block";
 import { useInstance } from '@milkdown/vue';
-import { usePluginViewContext } from '@prosemirror-adapter/vue';
 import { onUnmounted, ref, VNodeRef, watch } from 'vue';
 
-const { view } = usePluginViewContext()
 const [loading, get] = useInstance()
 
 const divRef = ref<VNodeRef>();
@@ -21,16 +19,9 @@ watch([loading], () => {
       content: divRef.value as any,
     })
 
-    tooltipProvider.update(view.value)
+    tooltipProvider.update()
   })
 })
-
-watch(
-  [view],
-  () => {
-    tooltipProvider?.update(view.value)
-  }
-)
 
 onUnmounted(() => {
   tooltipProvider?.destroy()
@@ -40,7 +31,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div ref="divRef" className="w-6 bg-slate-200 rounded hover:bg-slate-300 cursor-grab">
+  <div ref="divRef" className="w-6 bg-slate-200 rounded hover:bg-slate-300 cursor-grab absolute">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
     </svg>
