dc1fb8083a3fa04b81033ed38c460799851adb75
Saul-Mirone <Saul-Mirone@outlook.com>
2025-01-17 17:31:32 +0800

    chore: f

---
+++ bump.mjs
@@ -0,0 +1,25 @@
+import { exec } from "child_process";
+import fs from "fs";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import { promisify } from "util";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const exampleDirs = fs
+  .readdirSync(__dirname, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory() && !dirent.name.startsWith("."));
+
+await Promise.all(
+  exampleDirs.map(async (dir) => {
+    const { name } = dir;
+    const path = resolve(__dirname, name);
+    console.log(`[${name}]: Start bump.`);
+    const { stderr } = await promisify(exec)(
+      `cd ${path} && pnpm up @milkdown/* --latest`
+    );
+    if (stderr) console.error(stderr);
+    console.log(`[${name}]: Bump complete.`);
+  })
+);
--- vanilla-commonmark/src/main.ts
+++ vanilla-commonmark/src/main.ts
@@ -1,24 +1,22 @@
-import { defaultValueCtx, Editor, rootCtx } from '@milkdown/kit/core';
-import { commonmark } from '@milkdown/kit/preset/commonmark';
-import { nord } from '@milkdown/theme-nord';
+import { defaultValueCtx, Editor, rootCtx } from "@milkdown/kit/core";
+import { commonmark } from "@milkdown/kit/preset/commonmark";
+import { nord } from "@milkdown/theme-nord";
 
-import '@milkdown/theme-nord/style.css';
+import "@milkdown/theme-nord/style.css";
 
-import './style.css';
+import "./style.css";
 
-const markdown =
-`# Milkdown Vanilla Commonmark
+const markdown = `# Milkdown Vanilla Commonmark
 
 > You're scared of a world where you're needed.
 
-This is a demo for using Milkdown with **Vanilla Typescript**.`
+This is a demo for using Milkdown with **Vanilla Typescript**.`;
 
-Editor
-  .make()
-  .config(ctx => {
-    ctx.set(rootCtx, '#app')
-    ctx.set(defaultValueCtx, markdown)
+Editor.make()
+  .config((ctx) => {
+    ctx.set(rootCtx, "#app");
+    ctx.set(defaultValueCtx, markdown);
   })
   .config(nord)
   .use(commonmark)
-  .create()
+  .create();
