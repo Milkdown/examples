3261b982423e6de06a06d413baa0b344d535972e
Saul-Mirone <Saul-Mirone@outlook.com>
2025-02-13 18:12:41 +0800

    chore: fix slash

--- vanilla-slash/src/main.ts
+++ vanilla-slash/src/main.ts
@@ -1,38 +1,34 @@
-import { defaultValueCtx, Editor, rootCtx } from '@milkdown/kit/core';
-import { commonmark } from '@milkdown/kit/preset/commonmark';
-import { nord } from '@milkdown/theme-nord';
-import { slash, slashPluginView } from './Slash';
+import { defaultValueCtx, Editor, rootCtx } from "@milkdown/kit/core";
+import { commonmark } from "@milkdown/kit/preset/commonmark";
+import { nord } from "@milkdown/theme-nord";
+import { slash, slashPluginView } from "./Slash";
 
 declare global {
-  var __milkdown__: Editor
+  var __milkdown__: Editor;
 }
 
-import '@milkdown/theme-nord/style.css';
+import "@milkdown/theme-nord/style.css";
 
-import './style.css';
-
-const markdown =
-`# Milkdown Vanilla Slash
+const markdown = `# Milkdown Vanilla Slash
 
 > You're scared of a world where you're needed.
 
 This is a demo for using Milkdown with **Vanilla Typescript**.
 
-Type \`/\` to see the slash command.`
+Type \`/\` to see the slash command.`;
 
-Editor
-  .make()
-  .config(ctx => {
-    ctx.set(rootCtx, '#app')
-    ctx.set(defaultValueCtx, markdown)
+Editor.make()
+  .config((ctx) => {
+    ctx.set(rootCtx, "#app");
+    ctx.set(defaultValueCtx, markdown);
     ctx.set(slash.key, {
-      view: slashPluginView
-    })
+      view: slashPluginView,
+    });
   })
   .config(nord)
   .use(commonmark)
   .use(slash)
   .create()
-  .then(editor => {
-    globalThis.__milkdown__ = editor
-  })
+  .then((editor) => {
+    globalThis.__milkdown__ = editor;
+  });
