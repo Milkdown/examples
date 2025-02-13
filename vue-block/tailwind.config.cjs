c1a2bf468499aaef1a9c0f967aaa277f7c081a8f
Saul-Mirone <Saul-Mirone@outlook.com>
2025-02-13 17:37:30 +0800

    chore: fix vue block

--- vue-block/tailwind.config.cjs
+++ vue-block/tailwind.config.cjs
@@ -1,12 +1,9 @@
 /* Copyright 2021, Milkdown by Mirone. */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['**/*.tsx', '**/*.ts', '**/*.vue', '**/*.html'],
-  darkMode: 'class',
+  content: ["**/*.tsx", "**/*.ts", "**/*.html"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
-}
+};
--- vue-block/vite.config.ts
+++ vue-block/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), tailwindcss()],
+});
