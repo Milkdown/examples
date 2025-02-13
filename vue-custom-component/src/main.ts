82621e97e1186089b5dff6a7f86ac182c4345860
Saul-Mirone <Saul-Mirone@outlook.com>
2025-02-13 18:10:29 +0800

    chore: fix

--- vue-custom-component/src/main.ts
+++ vue-custom-component/src/main.ts
@@ -1,7 +1,5 @@
-import { createApp } from 'vue'
-import "@milkdown/theme-nord/style.css"
-import App from './App.vue'
+import { createApp } from "vue";
+import "@milkdown/theme-nord/style.css";
+import App from "./App.vue";
 
-import './style.css'
-
-createApp(App).mount('#app')
+createApp(App).mount("#app");
