31175168c867a6a706e7142c94a54e96269ac3be
Saul-Mirone <Saul-Mirone@outlook.com>
2025-06-16 11:11:05 +0800

    feat: bump tsconfig svelte

--- svelte-commonmark/src/main.ts
+++ svelte-commonmark/src/main.ts
@@ -3,7 +3,7 @@ import "@milkdown/theme-nord/style.css";
 import App from "./App.svelte";
 
 const app = mount(App, {
-  target: document.getElementById("app"),
+  target: document.getElementById("app")!,
 });
 
 export default app;
